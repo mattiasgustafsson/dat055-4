7883af19bb8021fad2c86787b4965df3286276fc
Mattias Gustafsson <mattg@student.chalmers.se>
2018-02-09 16:45:48 +0100

    Worked on showing food items in GUI

--- src/zombieinfection/controller/InventoryController.java
+++ src/zombieinfection/controller/InventoryController.java
@@ -1,22 +1,10 @@
 package zombieinfection.controller;
 
-
+import zombieinfection.model.*;
 
 public class InventoryController {
 	
 	public void foodSlotClicked(int nr) {
-		
-		if(nr == 1){
-			System.out.println("mat1 konsumerad");
-		}
-		if(nr == 2){
-			System.out.println("mat2 konsumerad");
-		}
-		if(nr == 3){
-			System.out.println("mat3 konsumerad");
-		}
-		if(nr == 4){
-			System.out.println("mat4 konsumerad");
-		}
+		System.out.println("Food slot " + nr + " was clicked.");
 	}
 }
--- src/zombieinfection/view/GUI/InventoryPanel.java
+++ src/zombieinfection/view/GUI/InventoryPanel.java
@@ -1,15 +1,20 @@
 package zombieinfection.view.GUI;
 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO; // TODO Used for icons so remove if end up not using icons
 import javax.swing.*;
 
-import zombieinfection.controller.InventoryController;
+import zombieinfection.controller.*;
+import zombieinfection.model.*;
 
 public class InventoryPanel extends JPanel {
 	JPanel keys = new JPanel(new GridLayout(1, 4));
 	JPanel food = new JPanel(new GridLayout(1, 4));
 	JPanel weapons = new JPanel(new GridLayout(1, 4));
 	JPanel ingredients = new JPanel(new GridLayout(1, 4));
+	JButton[] fbuttons;
 	
 	private static final long serialVersionUID = 4112124953987216988L;
 
@@ -26,18 +31,42 @@ public class InventoryPanel extends JPanel {
 		ingredients.add(new JButton("Slot7"));
 		ingredients.add(new JButton("Slot8"));
 		
-		JButton fbutton1 = new JButton("Food1"); // TODO Make loop great again
-		fbutton1.addActionListener(e -> {ic.foodSlotClicked(1);});
-		food.add(fbutton1);
-		JButton fbutton2 = new JButton("Food2");
-		fbutton2.addActionListener(e -> {ic.foodSlotClicked(2);});
-		food.add(fbutton2);
-		JButton fbutton3 = new JButton("Food3");
-		fbutton3.addActionListener(e -> {ic.foodSlotClicked(3);});
-		food.add(fbutton3);
-		JButton fbutton4 = new JButton("Food4");
-		fbutton4.addActionListener(e -> {ic.foodSlotClicked(4);});
-		food.add(fbutton4);
+		fbuttons = new JButton[4];
+		for (int i = 0; i < 4; i++) {
+			fbuttons[i] = new JButton("Food" + (i + 1));
+			food.add(fbuttons[i]);
+		}
+		
+		fbuttons[0].addActionListener(e -> {ic.foodSlotClicked(1);});
+		fbuttons[1].addActionListener(e -> {ic.foodSlotClicked(2);});
+		fbuttons[2].addActionListener(e -> {ic.foodSlotClicked(3);});
+		fbuttons[3].addActionListener(e -> {ic.foodSlotClicked(4);});
+		
+		File im = new File("appleIcon.png");
+		ImageIcon icon = new ImageIcon(im.getPath());
+		fbuttons[0].setIcon(icon);
+
+//		try {
+//			Image img = ImageIO.read(getClass().getResource("appleIcon.png"));
+//			fbuttons[0].setIcon(new ImageIcon(img));
+//		}
+//		catch (IOException e1) {
+//			e1.printStackTrace();
+//			System.out.println("Something went wrong with img import!");
+//		}
+		
+//		JButton fbutton1 = new JButton("Food1"); // TODO Make loop great again
+//		fbutton1.addActionListener(e -> {ic.foodSlotClicked(1);});
+//		food.add(fbutton1);
+//		JButton fbutton2 = new JButton("Food2");
+//		fbutton2.addActionListener(e -> {ic.foodSlotClicked(2);});
+//		food.add(fbutton2);
+//		JButton fbutton3 = new JButton("Food3");
+//		fbutton3.addActionListener(e -> {ic.foodSlotClicked(3);});
+//		food.add(fbutton3);
+//		JButton fbutton4 = new JButton("Food4");
+//		fbutton4.addActionListener(e -> {ic.foodSlotClicked(4);});
+//		food.add(fbutton4);
 
 		keys.add(new JButton("Key1"));
 		keys.add(new JButton("Key2"));
