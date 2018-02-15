f845a8fe242ed0a8c02cec0b2714a32131e31460
Legolas7 <guslindq@student.chalmers.se>
2018-02-15 15:45:57 +0100

    locked door

--- src/zombieinfection/view/GUI/Map.java
+++ src/zombieinfection/view/GUI/Map.java
@@ -31,24 +31,22 @@ import zombieinfection.model.Room;
 	        drawOneRoom(g, entry, 200, 300, drawnRooms);
 	    }
 
-		private void drawOneRoom(Graphics g, Room room, int x, int y, HashSet<Room> drawnRooms) {
-			 // if this room is already drawn, exit immediately!
-			if (drawnRooms.contains(room));
-			
-			//change color for the current room
-			if (room == currentRoom) {
-				g.setColor(Color.RED);
-			}
-			
-			//change color for the end room
-			else if (room == endRoom) {
-				g.setColor(Color.GREEN);
-			}
-			else {
-				g.setColor(Color.BLACK);
-			}
-
-			// draw the room - just a simple square
+	    private void drawOneRoom(Graphics g, Room room, int x, int y, HashSet<Room> drawnRooms) {
+	        // if this room is already drawn, exit immediately! otherwise: eternal loop...
+	        if (drawnRooms.contains(room)) return;
+	        
+	        // the current room is painted red, other rooms are painted black
+	        if (room == currentRoom) {
+	            g.setColor(Color.decode("#FF0040"));
+	        }
+	        else if (room == endRoom) {
+	            g.setColor(Color.decode("#04B45F"));
+	        }
+	        else {
+	            g.setColor(Color.BLACK);
+	        }
+	        
+	        // draw the room - just a simple square
 	        g.fillRect(x, y, 35, 35);
 	        
 	        // keep track of which rooms have been drawn
@@ -56,9 +54,31 @@ import zombieinfection.model.Room;
 	        
 	        // draw the exits - start by setting a black color
 	        g.setColor(Color.BLACK);
-			
-			
-			
-		}
+	        
+	        // for each possible direction, check if there is an exit
+	        if (room.hasExit("north")) {
+	            // draw the "door"
+	            g.fillRect(x + 15, y - 5, 5, 5);
+	            // then call drawOneRoom recursively with the next room's position
+	            drawOneRoom(g, room.getExit("north"), x, y - 40, drawnRooms);
+	        }
+	        
+	        if (room.hasExit("south")) {
+	        	//draw the door
+	            g.fillRect(x + 15, y + 35, 5, 5);
+	            
+	            drawOneRoom(g, room.getExit("south"), x, y + 40, drawnRooms);
+	        }
+	        
+	        if (room.hasExit("east")) {
+	            g.fillRect(x + 35, y + 15, 5, 5);
+	            drawOneRoom(g, room.getExit("east"), x + 40, y, drawnRooms);
+	        }
+	        
+	        if (room.hasExit("west")) {
+	            g.fillRect(x - 5, y + 15, 5, 5);
+	            drawOneRoom(g, room.getExit("west"), x - 40, y, drawnRooms);
+	        }
+	    }
 
 }
